ff33eb81b4d6d91adce31e31437636aeebb4ae92
Bohdan Balanyk <bbalanyk@deloitte.com>
2023-10-20 16:33:54 +0300

    add lab3 with report(initial version)

---
+++ lab3.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def gauss_elimination(A, b):
+    n = len(b)
+    for i in range(n):
+        # Вибір головного елементу по рядку
+        max_row = max(range(i, n), key=lambda r: abs(A[r][i]))
+        A[i], A[max_row] = A[max_row], A[i]
+        b[i], b[max_row] = b[max_row], b[i]
+        
+        # Обернення рядків A[i]
+        for j in range(i + 1, n):
+            factor = A[j][i] / A[i][i]
+            b[j] -= factor * b[i]
+            for k in range(i, n):
+                A[j][k] -= factor * A[i][k]
+
+    x = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - sum(A[i][j] * x[j] for j in range(i + 1, n))) / A[i][i]
+    return x
+
+def system(x):
+    x1, x2 = x
+    f1 = (x1**2 - x2**2)**2 / 4 - x1**2 + x2**2 + 0.5 - x1
+    f2 = x1 * x2 * (x1**2 - x2**2) + 0.5 - x2
+    return np.array([f1, f2])
+
+def jacobian(x):
+    x1, x2 = x
+    return np.array([
+        [2*(x1**2 - x2**2)*x1/4 - 2*x1 - 1, -2*(x1**2 - x2**2)*x2/4 + 2*x2],
+        [x2*(x1**2 - x2**2) + 2*x1*x2*x1, x1*(x1**2 - x2**2) - 2*x1*x2*x2 - 1]
+    ])
+
+def newton_method(x0, eps):
+    x = x0
+    while np.linalg.norm(system(x)) > eps:
+        J = jacobian(x)
+        F = system(x)
+        delta = gauss_elimination(J, -F)
+        x += delta
+    return x
+
+# Початкова точка наближення
+x0 = np.array([0.0, 0.0])
+# Відносна похибка
+epsilon = 1e-5
+# Знаходження розв'язку системи
+solution = newton_method(x0, epsilon)
+
+print("Solution:", solution)
