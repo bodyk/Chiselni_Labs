9bd6f5f864d0279d378f621fdc4786e4cec982f8
Bohdan Balanyk <bbalanyk@deloitte.com>
2023-10-20 09:55:13 +0300

    modify lab1 to stick to recommended algo steps

--- lab1.py
+++ lab1.py
@@ -1,46 +1,50 @@
 import numpy as np
 
-# Veriant 2
-# s=0.02×2=0.04
-# B=0.02×25=0.5
+# Вхідні дані
+k = 2
+p = 25
+s = 0.02 * k
+B = 0.02 * p
 
-# Система рівнянь після підстановки:
-# 8.3x1​+2.64x2​+4.1x3​+1.9x4​=−11.15
-# 3.92x1+8.45x2+7.74x3+2.46x4=12.21
-# 3.77x1+7.25x2+8.04x3+2.28x4=14.95
-# 2.21x1+3.61x2+1.69x3+6.69x4=−8.35
-
-# Матриця системи
-A = np.array([
-    [8.3, 2.64, 4.1, 1.9],
-    [3.92, 8.45, 7.74, 2.46],
-    [3.77, 7.25, 8.04, 2.28],
-    [2.21, 3.61, 1.69, 6.69]
+A_original = np.array([
+    [8.3, 2.6 + s, 4.1, 1.9],
+    [3.92, 8.45, 7.78 - s, 2.46],
+    [3.77, 7.21 + s, 8.04, 2.28],
+    [2.21, 3.65 - s, 1.69, 6.69]
 ])
+A = A_original.copy()
+b = np.array([-10.65 + B, 12.21, 15.45 - B, -8.35])
 
-# Вектор вільних членів
-b = np.array([-11.15, 12.21, 14.95, -8.35])
-
-n = len(b)
-
-# Прямий хід (з приведенням до верхньотрикутного вигляду)
-for k in range(n-1):
+# 1. Прямий хід методу
+n = A.shape[0]
+for k in range(n):
     # Вибір головного елемента по стовпцю
-    maxindex = abs(A[k:,k]).argmax() + k
-    if A[maxindex, k] == 0:
-        raise ValueError("Matrix is singular.")
+    max_el = abs(A[k][k])
+    max_row = k
+    for i in range(k+1, n):
+        if abs(A[i][k]) > max_el:
+            max_el = abs(A[i][k])
+            max_row = i
+            
     # Перестановка рядків
-    if maxindex != k:
-        A[[k,maxindex]] = A[[maxindex, k]]
-        b[[k,maxindex]] = b[[maxindex, k]]
-    for row in range(k+1, n):
-        multiplier = A[row][k]/A[k][k]
-        A[row][k:] = A[row][k:] - multiplier*A[k][k:]
-        b[row] = b[row] - multiplier*b[k]
-
-# Зворотний хід
+    A[[k, max_row]] = A[[max_row, k]]
+    b[k], b[max_row] = b[max_row], b[k]
+    
+    # Зведення до верхньої трикутної форми
+    for i in range(k+1, n):
+        factor = A[i][k] / A[k][k]
+        for j in range(k, n):
+            A[i][j] -= factor * A[k][j]
+        b[i] -= factor * b[k]
+        
+# 2. Обернений хід
 x = np.zeros(n)
-for k in range(n-1, -1, -1):
-    x[k] = (b[k] - np.dot(A[k, k+1:], x[k+1:]))/A[k, k]
+for i in range(n-1, -1, -1):
+    x[i] = b[i]
+    for j in range(i+1, n):
+        x[i] -= A[i][j] * x[j]
+    x[i] /= A[i][i]
 
-print(x)
+print("Розв'язок системи: ", x)
+print("Перевірка (Ax): ", np.dot(A_original, x))
+print("Вектор вільних членів b: ", b)
